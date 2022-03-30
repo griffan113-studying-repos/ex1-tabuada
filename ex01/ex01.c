fa146579e48b87bc220b3ef433b1d684a212caa9
Eliel <elielmichel@gmail.com>
2022-03-29 22:35:43 -0300

    so numeros pares

--- ex01/ex01.c
+++ ex01/ex01.c
@@ -3,10 +3,12 @@
 int main() {
 	printf("Tabuada: \n");
 
-	for (int index = 1; index <= 10; index++) {
+	for (int index = 2; index <= 10;) {
 		for (int current_number = 0; current_number <= 10; current_number++) {
 			printf("%ix%i = %i\n", index, current_number, index * current_number);
 		}
+
+		index += 2;
 	}
 
 	return 0;
