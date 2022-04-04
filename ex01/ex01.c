a73b7b23b6db2e2d62b70c6d8e8680af9a701ff9
Eliel <elielmichel@gmail.com>
2022-04-04 20:06:53 -0300

    Adicionar if no fim de cada iteração

--- ex01/ex01.c
+++ ex01/ex01.c
@@ -2,11 +2,14 @@
 
 int main() {
 	printf("Tabuada: \n");
-
+	
 	for (int index = 1; index <= 10; index++) {
 		for (int current_number = 0; current_number <= 10; current_number++) {
 			printf("%ix%i = %i\n", index, current_number, index * current_number);
+
+			if (current_number == 10) printf("\n");
 		}
+
 	}
 
 	return 0;
